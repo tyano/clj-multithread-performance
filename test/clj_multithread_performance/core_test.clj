d331b9daadcdf3e71c12ecffcbde4a6122f371e8
Tsutomu YANO <t_yano@me.com>
2017-12-09 16:01:10 +0900

    create a new project.

---
+++ test/clj_multithread_performance/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-multithread-performance.core-test
+  (:require [clojure.test :refer :all]
+            [clj-multithread-performance.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
