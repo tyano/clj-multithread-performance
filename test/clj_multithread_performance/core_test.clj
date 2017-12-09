64608305eded89072b918a3ba7a0d83c68ee20ed
Tsutomu YANO <t_yano@me.com>
2017-12-09 16:02:01 +0900

    removed unused files

--- test/clj_multithread_performance/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-multithread-performance.core-test
-  (:require [clojure.test :refer :all]
-            [clj-multithread-performance.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
